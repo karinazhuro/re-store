e1f421f8a19cfb1e05b64b9168ac7672288fb385
Zhuro <kzhuro36@gmail.com>
2021-03-31 15:15:33 +0300

    feat: implement carcass in app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
+import {BrowserRouter as Router} from "react-router-dom";
+
 import App from "./components/app";
+import ErrorBoundary from "./components/error-boundry";
+import BookstoreService from "./services/bookstore-services";
+import {BookstoreServicesProvider} from './components/bookstore-service-context';
+
+import store from "./store";
+
+const bookstoreService = new BookstoreService();
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+			<ErrorBoundary>
+				<BookstoreServicesProvider value={bookstoreService}>
+					<Router>
+						<App />
+					</Router>
+				</BookstoreServicesProvider>
+			</ErrorBoundary>
+		</Provider>,
   document.getElementById('root')
 );
